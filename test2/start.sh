d761a3b6ae09c055925830681f245ea1d565de2d
Christian Zufferey <christian@zufferey.com>
2018-12-18 14:48:32 +0100

    wip

--- test2/start.sh
+++ test2/start.sh
@@ -1,5 +1,7 @@
 #Petit script pour lancer le binz
-#zf181218.1425
+#zf181218.1448
+#source: https://hub.docker.com/r/selenium/standalone-chrome/dockerfile
+
 
 docker run -d -v `pwd`:/dev/shm --name="selenium-chrome" selenium/standalone-chrome
 
