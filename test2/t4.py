f802cb14a7833a6a2a960d2a72a1e4ab01337103
Christian Zufferey <christian@zufferey.com>
2018-12-18 10:32:11 +0100

    wip

--- test2/t4.py
+++ test2/t4.py
@@ -4,11 +4,11 @@ import time
 from selenium import webdriver
 browser = webdriver.Chrome()
 
-browser.get('https://www.epfl.ch/')
+browser.get('http://www.melaniezufferey.ch')
 browser.set_window_position(0, 0)
 browser.set_window_size(1200, 4800)
 #wait for the site to adapt
-time.sleep(5.3)
+time.sleep(3.3)
 browser.save_screenshot('toto.png')
 browser.quit()
 
